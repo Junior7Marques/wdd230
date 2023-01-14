235514f7b792997b43189220b4c65f6559d838ef
Junior7Marques <juniorjr07@live.com>
2023-01-14 09:36:34 -0300

    changing js

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,3 @@ const lastupdate = document.querySelector('#lastupdate');
 const options1 = {month: 'numeric',day: 'numeric', year: 'numeric' , hour:'numeric', minute: 'numeric'};
 let oLastModif = new Date(document.lastModified).toLocaleDateString('en-US', options1);
 lastupdate.textContent = `${oLastModif}`
-
-const options = {weekday: 'short', day: 'numeric', month: 'short', year: 'numeric'};
-document.getElementById('current').textContent = new Date().toLocaleDateString('en-US', options)
